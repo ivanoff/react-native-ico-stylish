986394a92ac8da7000a6ead9076ec9b2063bd94d
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:40 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -202,10 +202,35 @@ declare module 'react-native-ico-stylish' {
       'zip' |
       'zipper-tool';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -213,4 +238,3 @@ declare module 'react-native-ico-stylish' {
     export { iconNames };
     export default Icon;
 }
-
