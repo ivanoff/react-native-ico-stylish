35e8d8e3cd8a635e1bbf6247ea0badc1abd5806a
Dimitry Ivanov <2@ivanoff.org.ua>
2023-10-01 18:37:08 +0300

    fix types

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-native-ico-stylish' {
     import { ReactNode } from 'react';
+    import { SvgProps, AdditionalProps } from 'react-native-svg';
 
     type iconNames = 'bikini-one-piece-swimwear' |
       'bikini-shape-1' |
@@ -233,7 +234,7 @@ declare module 'react-native-ico-stylish' {
       badge?: number | string | badgeTypeObj;
     }
 
-    const Icon: (props: IconProps) => ReactNode;
+    const Icon: (props: IconProps & SvgProps & AdditionalProps) => ReactNode;
 
     export { iconNames };
     export default Icon;
